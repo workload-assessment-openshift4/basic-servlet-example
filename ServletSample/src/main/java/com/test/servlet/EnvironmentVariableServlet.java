b9cba1f119ccb4a508f85a4d0b61e34c81bf738f
Fola <folaoso23@gmail.com>
2020-12-10 16:00:47 -0500

    Added Servlet for testing environment variable additions

---
+++ ServletSample/src/main/java/com/test/servlet/EnvironmentVariableServlet.java
@@ -0,0 +1,35 @@
+package com.test.servlet;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EnvironmentVariableServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public EnvironmentVariableServlet() {
+        super();
+        System.out.println("EnvironmentVariableServlet constructor called");
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("EnvironmentVariableServlet \"Init\" method called");
+    }
+
+    public void destroy() {
+        System.out.println("EnvironmentVariableServlet \"Destroy\" method called");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("Username and email id is retrived from the session");
+        HttpSession session = request.getSession(false);
+        PrintWriter out = response.getWriter();
+        out.write("<html><body><h1>This is the environment stuff.</h1></body></html>");
+    }
+}
