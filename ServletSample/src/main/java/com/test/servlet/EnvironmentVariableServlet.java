3259ea4256ba158214e3de778a76c26399eda7a1
Fola <folaoso23@gmail.com>
2020-12-10 22:44:51 -0500

    Added catalina property

--- ServletSample/src/main/java/com/test/servlet/EnvironmentVariableServlet.java
+++ ServletSample/src/main/java/com/test/servlet/EnvironmentVariableServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Properties;
 
 public class EnvironmentVariableServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -32,5 +32,17 @@ public class EnvironmentVariableServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         String testEnv = System.getenv("TEST_ENV");
         out.write("<html><body><h1>This is the environment variable: " + testEnv + "</h1></body></html>");
+
+        String catalinaConfigDirectory = System.getenv("CATALINA_HOME") + "/conf/";
+        File catalinaProperties = new File(catalinaConfigDirectory,"catalina.properties");
+        InputStream stream = new FileInputStream(catalinaProperties);
+        Properties props = new Properties();
+        props.load(stream);
+
+        //This can be any key-value pair in the properties file
+        String catProp = props.getProperty("common.loader");
+        out.write("<html><body><h1>This is a property from catalina.properties: " + catProp + "</h1></body></html>");
+
+
     }
 }
